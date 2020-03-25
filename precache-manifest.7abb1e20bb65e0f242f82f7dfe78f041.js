3eabd6f5baa6f366e800f2ffa0eea3723464ca5e
singh <singh@appysphere.com>
2020-03-25 20:36:07 +0700

    Updates

---
+++ precache-manifest.7abb1e20bb65e0f242f82f7dfe78f041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b0ffabd6fc3198e1967cbd69bce849",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "794d4ca76c3a1b1ab905",
+    "url": "/monsters-rolodex/static/css/main.9fd6477c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794d4ca76c3a1b1ab905",
+    "url": "/monsters-rolodex/static/js/main.e4975ad7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
